e5f11738cbf84fe7f53c1a45bbd8b5aca018eb9c
candy-Tong <563378816@qq.com>
2017-02-06 00:16:07 +0800

    标签未能正确显示

--- advanced/common/config/params.php
+++ advanced/common/config/params.php
@@ -3,5 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'upload_url'=>'candycute.cn/imooc/Yii_blog/advanced/frontend/web',
+    'upload_url'=>'http://candycute.cn/imooc/Yii_blog/advanced/frontend/web',
 ];
